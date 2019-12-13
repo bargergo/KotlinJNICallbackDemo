116c376f9e97521bc77908d0b15e238592f9818b
Gergely Baranyai <bargergo@gmail.com>
2019-12-13 17:12:10 +0100

    Created project with layout

---
+++ app/src/main/java/hu/example/kotlinjnicallbackdemo/MainActivity.kt
@@ -0,0 +1,34 @@
+package hu.example.kotlinjnicallbackdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnStart.setOnClickListener {
+            tvStartValue.text = stringFromJNI()
+        }
+        btnClap.setOnClickListener {
+            tvEndValue.text = stringFromJNI()
+        }
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
